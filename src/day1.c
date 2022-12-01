b60ee29ccee6c9a889354c0f407ea0335f950804
Daniel Berg <mail@roosta.sh>
2022-12-01 20:28:53 +0100

    day1: refactor
    store input in array, so that we can call multiple functions

--- src/day1.c
+++ src/day1.c
@@ -3,11 +3,16 @@
 #include <stdlib.h>
 
 #define BUFSIZE 16
-#define MAXLINE 16
+#define MAXLEN 2500
+
+char buf[MAXLEN][BUFSIZE];
+int length[MAXLEN];
+int height;
 
 int get_line(char s[]) {
   int c, i;
-  for (i = 0, c = getchar(); i < BUFSIZE - 1 && c != EOF && c != '\n'; ++i, c = getchar()) {
+  c = getchar();
+  for (i = 0; i < BUFSIZE - 1 && c != EOF && c != '\n'; ++i, c = getchar()) {
     s[i] = c;
   }
   if (c == '\n')
@@ -17,14 +22,15 @@ int get_line(char s[]) {
 }
 
 void mostcal(void) {
-  char buf[MAXLINE];
   long number, high;
   long result = high = 0;
   char *endptr;
-  while (get_line(buf) > 0) {
-    number = strtol(buf, &endptr, 10);
+  char *str;
+  for (int i = 0; i < height; i++) {
+    str = buf[i];
+    number = strtol(str, &endptr, 10);
     result += number;
-    if (endptr == buf) {
+    if (endptr == str) {
       if (result > high) high = result;
       result = 0;
       continue;
@@ -33,7 +39,9 @@ void mostcal(void) {
   printf("most calories: %ld\n", high);
 }
 
-
 int main() {
+  for (height = 0; (length[height] = get_line(buf[height])) > 0; height++)
+    ;
+
   mostcal();
 }
