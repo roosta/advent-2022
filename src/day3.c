5790d693bc6a93daad0159388828dfc36f41eb4b
Daniel Berg <mail@roosta.sh>
2022-12-07 17:22:28 +0100

    day3: add part 2 solution

--- src/day3.c
+++ src/day3.c
@@ -2,10 +2,12 @@
 #include <ctype.h>
 #include "advent.h"
 
+// Return character priority
 int get_pri(char ch) {
   return isupper(ch) ? ch - 38 : ch - 96;
 }
 
+// Compare two halves of a string for matching char
 char compare(char line[], int length) {
   for (int i = 0; i < length / 2; i++) {
     for (int j = length / 2; j < length; j++) {
@@ -16,7 +18,40 @@ char compare(char line[], int length) {
   return 0;
 }
 
-int calc(void) {
+// Looks for target in a given line[idx]
+int seek(char target, int idx) {
+  char active;
+  for (int i = 0; i < length[idx]; i++) {
+    active = buf[idx][i];
+    if (active == target) return active;
+  }
+  return 0;
+}
+
+// Checks each char in first position against two more lines
+int compare3(int i) {
+  for (int j = 0; j < length[i]; j++) {
+    if (seek(buf[i][j], i + 1) && seek(buf[i][j], i + 2)) {
+      return buf[i][j];
+    }
+  }
+  return 0;
+}
+
+// Sum of badges priority
+int part2(void) {
+  int match, result;
+  result = 0;
+  for (int i = 0; i < height; i += 3) {
+    if ((match = compare3(i))) {
+      result += get_pri(match);
+    }
+  }
+  return result;
+}
+
+// Sum of matching item in line
+int part1(void) {
   int match, result;
   result = 0;
   for (int i = 0; i < height; i++) {
@@ -29,6 +64,7 @@ int calc(void) {
 
 int main() {
   readbuf(); // read in buffer
-  printf("Sum of matches is %d\n", calc());
+  printf("Part one: sum of matches is %d\n", part1());
+  printf("Part two: sum of badges is %d\n", part2());
   return 0;
 }
